ae91781829e0b86a5fec2a703957b65fed37b728
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2020-10-26 20:21:07 -0300

    Position Logic

---
+++ game.hs
@@ -0,0 +1,73 @@
+﻿import Maps
+import Util
+import Position
+import Tiringa
+import WereWolf
+import System.Process
+import Data.Matrix
+import System.Random
+
+prepare :: Matrix Char -> IO ()
+prepare m = do
+    let wp = findElementMatrix 'W' m (Data.Matrix.nrows m) :: (Int,Int)
+    start m wp
+
+start :: Matrix Char -> (Int, Int) -> IO ()
+start m wereWolfPos = do
+
+    -- Select Direction
+    system ("cls")
+    print m
+    putStr "Select a direction: "
+    dir <- getLine
+
+    -- Tiringa Movement
+    let [oldP, newP] = selectDirection m dir 'T'
+    let (message1, contin1) = mapper m newP wereWolfPos
+    let matrix = moveTiringa m oldP newP
+    if (contin1) then do
+        -- WereWolf Movement
+        let currPos = findElementMatrix 'W' m (Data.Matrix.nrows m) :: (Int,Int)
+        let possMov = getPossibleMoves matrix wereWolfPos currPos :: [(Int,Int)]
+        n <- randomRIO (0, (Prelude.length possMov) - 1) :: IO Int
+        let newPosWereWolf = wereWolfPosition currPos n possMov
+        let (message2, contin2) = mapper m newP newPosWereWolf
+        let newMatrix = moveWereWolf matrix currPos newPosWereWolf
+        if (contin2) then do
+            -- Print Matrix
+            print newMatrix
+            start newMatrix currPos
+        else do
+            system ("cls")
+            print newMatrix
+            print message2
+    else do
+        system ("cls")
+        print matrix
+        print message1
+
+wereWolfPosition :: (Int, Int) -> Int -> [(Int,Int)] -> (Int, Int)
+wereWolfPosition lastPosition n possMov | (Prelude.length possMov) == 0 = lastPosition
+                                   | otherwise = (possMov !! n)
+
+selectDirection :: Matrix Char -> String -> Char -> [(Int, Int)]
+selectDirection m dir e | (dir == "w") || (dir == "W") = [p] ++ [moveUp m p]
+                        | (dir == "s") || (dir == "S") = [p] ++ [moveDown m p]
+                        | (dir == "a") || (dir == "A") = [p] ++ [moveLeft m p]
+                        | (dir == "d") || (dir == "D") = [p] ++ [moveRight m p]
+                        | otherwise = [p,p]
+                        where p = findElementMatrix e m (Data.Matrix.nrows m)
+
+mapper :: Matrix Char -> (Int, Int) -> (Int, Int) -> (String, Bool)
+mapper m posTir posWW | (lostTheGame posTir posWW) = ("Game Over!", False)
+                      | wonTheGame m posTir = ("You Won! Congratulations!", False)
+                      | otherwise = ("", True)
+
+wonTheGame :: Matrix Char -> (Int, Int) -> Bool
+wonTheGame m (tx,ty) = (tx == sx) && (ty == sy)
+                       where (sx,sy) = findElementMatrix 'S' m (Data.Matrix.nrows m)
+
+lostTheGame :: (Int, Int) -> (Int, Int) -> Bool
+lostTheGame (tx, ty) (wx,wy) = (tx == wx) && (ty == wy) 
+
+
---
+++ wereWolf.hs
@@ -0,0 +1,26 @@
+﻿module WereWolf where
+import Data.Matrix
+import Position
+import Util
+import Data.Vector
+import Maps
+
+moveWereWolf :: Matrix Char -> (Int, Int) -> (Int,Int) -> Matrix Char
+moveWereWolf m old new = setElem 'W' new (setElem ' ' old m)
+
+getPossibleMoves :: Matrix Char -> (Int, Int) -> (Int, Int) -> [(Int, Int)]
+getPossibleMoves m (ox,oy) (cx,cy) =  [(x,y) | (x,y) <- (allPositions m (cx,cy)), 
+                                    ((not $ checkIsExit m (x,y)) 
+                                    && (not $ checkisEqual (x,y) (ox,oy))
+                                    && (not $ checkisEqual (x,y) (cx,cy)))]
+
+allPositions :: Matrix Char -> (Int, Int) -> [(Int, Int)]
+allPositions m (x, y) = [moveUp m (x,y), moveDown m (x,y),
+                         moveRight m (x,y), moveLeft m (x,y)]
+
+checkisEqual :: (Int, Int) -> (Int, Int) -> Bool
+checkisEqual (a,b) (c,d) = (a == c && b == d)
+
+checkIsExit :: Matrix Char -> (Int, Int) -> Bool
+checkIsExit m (r, c) | getElem r c m == 'S' = True
+                     | otherwise = False
