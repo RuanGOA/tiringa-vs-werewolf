ef412424e8747d679b5a8aeade0147820fac2335
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2020-10-28 10:57:09 -0300

    Movement  Optimization

---
+++ test.hs
@@ -0,0 +1,6 @@
+﻿euclideanDistance :: (Int, Int) -> (Int, Int) -> (Int, Int, Float)
+euclideanDistance (wx, wy) (tx, ty) = (wx, wy, sqrt (((wxf - txf) ** 2) + ((wyf - tyf) ** 2)))
+                                      where wxf = fromInteger $ toInteger wx :: Float
+                                            wyf = fromInteger $ toInteger wy :: Float
+                                            txf = fromInteger $ toInteger tx :: Float
+                                            tyf = fromInteger $ toInteger ty :: Float
